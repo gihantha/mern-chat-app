ed63d8ca497b9f32f0c0a6e4b2270ccac5b2028d
Kaveen <gihantha01@gmail.com>
2025-09-08 09:34:46 +0530

    signup logic implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+
 
 dotenv.config();
 
@@ -9,7 +11,9 @@ const app  = express();
 
 const PORT = process.env.PORT
 
-app.use(express.json);
+app.use(express.json());
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
 
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,4 @@
-import mongoose from "mongose";
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
     {
