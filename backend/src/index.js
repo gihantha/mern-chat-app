41d9050abf9d0fa87d507e719d69bd96abc76388
Kaveen <gihantha01@gmail.com>
2025-09-12 12:23:37 +0530

    fixing some issues with deployment..

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
     
     // FIX: Use proper wildcard pattern
-    app.get("/*", (req, res) => {
+    app.get('/*\w', (req, res) => {
         res.sendFile(path.resolve(__dirname, "../", "frontend", "dist", "index.html"));
     });
 
