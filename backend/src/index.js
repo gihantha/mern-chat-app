a76af5360f49ec501d400189faac30186e6afa31
Kaveen <gihantha01@gmail.com>
2025-09-12 10:07:48 +0530

    Merge React app and Node.js API for simplified deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
+import path from "path";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./lib/socket.js";
@@ -10,6 +11,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(express.json());
 
@@ -25,6 +27,16 @@ app.use(
 app.use("/api/auth", authRoutes);
 
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+      res.sendFile(
+        path.resolve(__dirname, "../", "frontend", "dist", "index.html")
+      );
+    });
+}
 
 server.listen(PORT, () => {
   console.log("Server is running on port :" + PORT);
